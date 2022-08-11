2eab885c80b949fdc04efa73d9130d1ea83fa696
Aditya <adityalahane31@gmail.com>
2022-08-12 00:30:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"; 
+import Header from "./components/Header";
+import "./App.css"
+import Template from "./components/Template";
+import MainBody from "./components/MainBody"; 
+import { BrowserRouter as Router ,Switch, Route } from "react-router-dom"; 
+import Formheader from "./components/Formheader"
+import Centeredtabs from "./components/Tabs";
+import Question_form from "./components/Question_form";
+import Userform from "./components/Userform"; 
+import Survey from "./components/Survey";
+import Worksheet from "./components/Worksheet";
+import Save from "./components/Save";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/form/:id">
+            <Formheader/> 
+            <Centeredtabs/> 
+            {/* <Question_form/>  */}
+          </Route>
+          <Route path="/contactinfo/:id">
+           <Survey />
+           
+          </Route>
+          <Route path="/Worksheet/:id">
+           <Worksheet />
+           
+          </Route>
+          <Route path="/Save/:id">
+           <Save />
+           
+          </Route>
+          <Route path="/response">
+            <Userform/> 
+            </Route> 
+            <Route path="/">  
+      <Header/>
+      <Template/> 
+      <MainBody/>                 
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
